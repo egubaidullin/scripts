58101e09173c11fc21de1171360e393817518830
EG <96171393+egubaidullin@users.noreply.github.com>
2023-09-10 14:14:29 +0700

    Update tlg_login_notify.sh

--- tlg_login_notify.sh
+++ tlg_login_notify.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # Check if the file /etc/ssh/notify-on-ssh-login.sh exists
-if [ -f /etc/ssh/notify-on-ssh-login.sh ]; then
-    # If the file exists, print a warning
-    echo "The file /etc/ssh/notify-on-ssh-login.sh already exists!"
-else
-    # If the file does not exist, create it with the given content
-    cat > /etc/ssh/notify-on-ssh-login.sh << EOF
-TELEGRAM_TOKEN="YourTelegramToken"
-CHAT_ID="YourChatID"
+if [ ! -f /etc/ssh/notify-on-ssh-login.sh ]; then
+    # Create the file with the given code and make it executable
+    cat << EOF > /etc/ssh/notify-on-ssh-login.sh
+#!/usr/bin/env bash
+
+TELEGRAM_TOKEN="YOUR_TELEGRAM_TOKEN"
+CHAT_ID="_YOUR_CHAT_ID"
 URL="https://api.telegram.org/bot\$TELEGRAM_TOKEN/sendMessage"
 
 if [ "\$PAM_TYPE" != "open_session" ]
 then
-	exit 0
+        exit 0
 else
-	curl -s -X POST \$URL -d chat_id=\$CHAT_ID -d text="\$(echo -e "Host: \`hostname\`\nUser: \$PAM_USER\nHost: \$PAM_RHOST")" > /dev/null 2>&1
-	exit 0
+        curl -s -X POST \$URL -d chat_id=\$CHAT_ID -d text="\$(echo -e "Time: \$(date +'%d/%m/%Y %H:%M:%S') (\$(date +'%Z %z'))\nHost: \`hostname\`\nUser: \$PAM_USER\nHost: \$PAM_RHOST")" -d disable_notification=true  > /dev/null 2>&1
+        exit 0
 fi
 EOF
-    # Make the file executable with permissions -rwxr-xr-x
+
     chmod 755 /etc/ssh/notify-on-ssh-login.sh
-fi
-
-# Check if the line "session    optional     pam_exec.so  /usr/local/bin/notify-on-ssh-login.sh" is in the file /etc/pam.d/sshd
-if grep -q "session    optional     pam_exec.so  /usr/local/bin/notify-on-ssh-login.sh" /etc/pam.d/sshd; then
-    # If the line is there, print a message
-    echo "The line is already present in the file /etc/pam.d/sshd!"
+    echo "File /etc/ssh/notify-on-ssh-login.sh created successfully."
 else
-    # If the line is not there, append it to the end of the file
-    echo "session    optional     pam_exec.so  /usr/local/bin/notify-on-ssh-login.sh" >> /etc/pam.d/sshd
+    echo "Warning: File /etc/ssh/notify-on-ssh-login.sh already exists. Skipping creation."
 fi
 
-# Finish the script
-exit 0
+# Check if the file /etc/pam.d/sshd contains the line "session    optional     pam_exec.so  /etc/ssh/notify-on-ssh-login.sh"
+if ! grep -q "session    optional     pam_exec.so  /etc/ssh/notify-on-ssh-login.sh" /etc/pam.d/sshd; then
+    # Append the line to the end of the file
+    echo "# Telegram notify on ssh login" >> /etc/pam.d/sshd
+    echo "session    optional     pam_exec.so  /etc/ssh/notify-on-ssh-login.sh" >> /etc/pam.d/sshd
+    echo "Line added to /etc/pam.d/sshd successfully."
+else
+    echo "Warning: Line already present in /etc/pam.d/sshd. Skipping addition."
+fi
